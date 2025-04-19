72c22032f015f977b654ff6edea980b7406f62d8
Aleksandr Tepteev <al_tepteev@vk.com>
2025-04-19 21:29:04 +0500

    Поправил конфиг  eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,25 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2021,
+    sourceType: "module"
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  plugins: ["vue", "@typescript-eslint"],
   rules: {
-    // остальные ваши правила
-    'vue/multi-word-component-names': 'off'
+    // Пример: можно настроить под себя
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "no-console": "warn"
   }
-}
+};
